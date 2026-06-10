a529f18ab43ec4280847b827151c0615ec08c604
SatFinder Dev <satfinder@example.com>
2026-06-10 21:01:05 +0000

    fix: 添加kotlin.plugin.compose插件，移除废弃的composeOptions配置
    Kotlin 2.0+ 需要使用 org.jetbrains.kotlin.plugin.compose 插件

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -36,10 +37,6 @@ android {
 
     buildFeatures {
         compose = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
     }
 }
 
